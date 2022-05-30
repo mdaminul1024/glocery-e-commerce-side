a7fa2cb6dd4182c78d7c9c16871dab229944f7d0
mdaminul1024 <mdfahad1024@gmail.com>
2022-05-31 00:06:33 +0600

    new push

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 }
+
+let shoppingCart = document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick = () => {
+    shoppingCart.classList.toggle('active');
+}
