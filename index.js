41f2933bcbc7bcda6f5f6a3d9e17175cd793cf1f
mdaminul1024 <mdfahad1024@gmail.com>
2022-06-03 00:25:04 +0600

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 let searchForm = document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    menuBar.classList.remove('active');
 }
 
 let shoppingCart = document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick = () => {
     shoppingCart.classList.toggle('active');
+    loginForm.classList.remove('active');
+    menuBar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 
 let loginForm = document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick = () =>{
     loginForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    menuBar.classList.remove('active');
+    searchForm.classList.remove('active');
 }
 let menuBar = document.querySelector('.navbar');
 document.querySelector('#menu-btn').onclick = () =>{
     menuBar.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    searchForm.classList.remove('active');
 }
